2da36af04bbd1bbb860d495b6948fe38260b8fea
rjtj2007 <teacherman1@gmail.com>
2018-03-26 21:26:34 -0700

    added file basics and some dependencies

--- lab-ryan/index.js
+++ lab-ryan/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 const express = require('express');
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
 
 const app = express();
 app.use(express.static('static'));
+
+const photosRouter = require('./router/photos.js');
+app.use()
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
