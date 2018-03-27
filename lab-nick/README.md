fdf26f5d536dc05b365e5db6c2b2d0f90f74151f
dambergn <dambergn@gmail.com>
2018-03-27 12:59:13 -0700

    basic file upload complete.

---
+++ lab-nick/README.md
@@ -0,0 +1,3 @@
+create a .env file inside the lab-nick folder.
+PORT='desired port'
+MONGODB=
--- lab-nick/index.js
+++ lab-nick/index.js
@@ -4,12 +4,18 @@ require('dotenv').config();
 
 const express = require('express');
 const multer = require('multer');
+const upload = multer({ dest: 'uploads/'});
 
 const app = express();
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
+});
+
+app.post('/photos/upload', upload.single('polaroid'), (req, res) => {
+  console.log('GOT', req.file);
+  res.send(req.file);
 });
 
 app.listen(process.env.PORT, () => {
