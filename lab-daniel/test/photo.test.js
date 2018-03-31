0ce7ce47b40e8e01bdc1eb767159629746bdfa30
daniel <dpillay03@gmail.com>
2018-03-31 12:13:30 -0700

    created test scaffolding

---
+++ lab-daniel/test/photo.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const agent = require('superagent');
+const photo = require('../router/photoRoute');
+
+describe('photoRoute()', () => {
+    it('checks if the upload worked and a resource object is returned', (done) => {
+        
+    })
+
+    it('returns all the resources that have been uploaded', (done) => {
+        
+    })
+
+    it('returns info on one resource that\'s been uploaded', (done) => {
+        
+    })
+})
