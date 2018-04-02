610c29390c91361f4e66bf3c0ba781c5c88adc48
ixnp <enprocopios@gmail.com>
2018-04-01 20:58:53 -0700

    server up file sent

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
+const multer = require('multer');
+
 const app = express();
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('http://localhost:'+ process.env.PORT);
 });
